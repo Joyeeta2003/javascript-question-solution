799c146e545d7deb906ce0e5e1238e182f36a2d3
joyeeta <joyeetadas597@gmail.com>
2026-02-26 17:28:06 +0530

    first 5 question done

---
+++ basic console usage/qn1.js
@@ -0,0 +1,3 @@
+let name = "joyee"
+let hobby = "listening song"
+console.log(`my name is ${name} and my hobby is ${hobby}`);
---
+++ basic console usage/qn4.js
@@ -0,0 +1,3 @@
+let firstName="Joyeeta"
+let lastName="Das"
+console.log(`${firstName} ${lastName}`);
